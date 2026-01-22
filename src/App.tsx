1cc0cce635e20396f0ae152cdfee95b8b7897691
Emerson Moreno <emerson.dev.machado@gmail.com>
2026-01-22 19:34:56 -0300

    feat: add tanstack router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { ComponentExample } from "@/components/component-example"
+import { routeTree } from "@/routeTree.gen"
+import { createRouter, RouterProvider } from "@tanstack/react-router"
 import { ThemeProvider } from "./components/theme-provider"
+
+const router = createRouter({
+  routeTree,
+  basepath: "/gestao-solo",
+});
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 export function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="gestao-solo-theme">
-      <ComponentExample />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/gestao-solo/",
+  plugins: [
+    tailwindcss(),
+    tanstackRouter({ target: "react", autoCodeSplitting: true }),
+    react(),
+  ],
+  base: "/gestao-solo",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
